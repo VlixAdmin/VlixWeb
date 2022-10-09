f44f0ba0772728e05440c0273122d175c5321544
Azrin Sani <azrinsani@gmail.com>
2022-10-09 18:45:41 +0800

    Add theme query param

--- js/pro-script.js
+++ js/pro-script.js
@@ -10,8 +10,4 @@ window.addEventListener('load', function () {
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
-// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-let value = params.bg; // "some_value"
-console.log(value);
-let ddd = document.getElementById("background");
-console.log(ddd);
+document.getElementById("background").style.background = 'radial-gradient(circle, rgb(255, 255, 255) 0%, rgb(255, 255, 255) 85%, #' + params.theme + ' 100%) 50% 50% / cover no-repeat';
