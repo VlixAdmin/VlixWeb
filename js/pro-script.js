fa3fe94525431ecd80a68a53f36c524a73c9ee52
Azrin Sani <azrinsani@gmail.com>
2022-10-09 18:30:31 +0800

    Rename to pro-script.js

---
+++ js/pro-script.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('load', function () {
+    setTimeout(()=>{
+        const el = document.getElementsByTagName("body")[0];
+        el.style.opacity = "1";
+        console.log('loaded');
+    },100);
+});
+
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+let value = params.bg; // "some_value"
+console.log(value);
+let ddd = document.getElementById("background");
+console.log(ddd);
